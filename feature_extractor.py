475b0dde85fcf9e53d14540ad1ce21c0298cc66e
jessica-cruz-dev <jessica.cruz.dev@gmail.com>
2019-02-10 01:04:44 -0600

    Add Retry for connection errors

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,20 +1,26 @@
-import argparse # CLI library
+import argparse
 import requests
 import csv
 import os
 import re
 import sys
 from pathlib import Path
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
 # CLI accepting user input
-parser = argparse.ArgumentParser(prog='feature_extractor', description='Process image ids.')
-parser.add_argument('feature_file', action='store', type=str, help='File to be read in')
+parser = argparse.ArgumentParser(prog='feature_extractor',
+                                 description='Process image ids.')
+parser.add_argument('feature_file', action='store', type=str,
+                    help='File to be read in')
 parser.add_argument('size', action='store', type=str, help='size')
 args = parser.parse_args()
 
-input_file = os.path.join(".","inputs", args.feature_file)
+# Setting up user inputs
+input_file = os.path.join(''.'','inputs', args.feature_file)
 img_size = args.size
 
+# Checking if input file is present
 file = Path(input_file)
 try:
     file.resolve()
@@ -25,25 +31,27 @@ except FileNotFoundError:
 else:
     new_folder = args.feature_file[:-4]
 
-
-# main url
+# Core url
 url = "https://eol.jsc.nasa.gov/DatabaseImages"
 
-# creating directory with full permission in octal
+# Noted ids with _2 extenstion in name
+anomalous_ids = ['ISS002-E-5448', 'ISS002-E-5632', 'ISS002-E-5633',
+                 'ISS002-E-5634']
+
+# Creating all new directories with full permission in octal
 mode = 0o777
 
-outputs_dir = os.path.join(".","outputs")
-
 # Create an outputs folder if one doesn't exist
+outputs_dir = os.path.join('.','outputs')
 if not os.path.isdir(outputs_dir):
     os.makedirs(outputs_dir, mode=mode)
 
+# Alerts for folder duplicate
 output_path = os.path.join(outputs_dir, new_folder, "")
-
 try:
     os.makedirs(output_path, mode=mode)
 except:
-    print("\n*** ERROR: Cannot create folder with name " + new_folder +\
+    print("\n*** ERROR: Cannot create folder with name " + new_folder +
           " because the folder alread exists. ***")
     print("\nTerminating script.")
     sys.exit()
@@ -52,24 +60,22 @@ except:
 with open(input_file, 'r') as p:
         total_count = sum(1 for counter1 in p)
 
-
+# Intitial output message
 print("\nTotal images to be downloaded: " + str(total_count) + "\n")
 
-# counter for progress of image downloads
+# Counter for progress of image downloads
 count1 = 0
 
-# reads in csv, closes file
+# Begin reading input feature file
 with open(input_file, 'r') as f:
 
-    # reader object containes all csv values
+    # Reader object containes all csv values
     reader = csv.reader(f)
-
-    # row = a python list of values in each line of csv file
+    # row = a python list of all values in single row
     for row in reader:
-
-        # populates the current image information for url
+        # Populates the current image information for url
         img_id = row[0]
-        # regular expression isolates the mission name
+        # Regular expression isolates the mission name
         mission = re.findall(r'\w+',img_id)[0]
 
         # Accounting for varying mission names
@@ -86,25 +92,39 @@ with open(input_file, 'r') as f:
             elif img_size == 'large':
                 img_size = 'highres'
 
-        # url of specified image
-        img_url = "{}/{}/{}/{}/{}.JPG".format(url, abbrev, img_size, mission,\
+        # Account for the few images with extended name
+        if img_id in anomalous_ids:
+            img_id = img_id + '_2'
+
+        # Full url of current image
+        img_url = '{}/{}/{}/{}/{}.JPG'.format(url, abbrev, img_size, mission,
                                               img_id)
 
-        # GET request to site
-        response = requests.get(img_url)
+        # Retry to account for connection errors
+        s = requests.Session()
+        retries = Retry(total=5,
+                        backoff_factor=0.1,
+                        status_forcelist=[ 500, 502, 503, 504 ])
+        s.mount('https://', HTTPAdapter(max_retries=retries))
 
-        # 404 code = image not found
-        if response.status_code == 404:
-            print("ERROR 404: File not found for image id: " + img_id)
-            print(img_url)
-        # 200 code = image found and returned
-        elif response.status_code == 200:
-            count1 += 1
+        # Prints to screen the success or failure of download
+        try:
+            response = s.get(img_url)
+        except requests.exceptions.ConnectionError:
+            print(response.status_code)
+        else:
+            if response.status_code == 404: # Image not found
+                print("ERROR 404: File not found for image id: " + img_id)
+                print(img_url)
+            elif response.status_code == 200: # Image found and returned
+                # Print progress to screen
+                count1 += 1
+                print("Downloaded image " + str(count1) + " / " +
+                      str(total_count))
 
-            print("Downloaded " + str(count1) + " / " + str(total_count))
-            # creates image path and name
-            output_file = "{}{}.jpg".format(output_path, img_id)
+                # Creates image path and name
+                output_file = '{}{}.jpg'.format(output_path, img_id)
 
-            # writes image to output file
-            with open(output_file, 'wb') as i:
-                i.write(response.content)
+                # Writes image to output file
+                with open(output_file, 'wb') as i:
+                    i.write(response.content)
